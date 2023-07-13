47f5767e60284a069171c64ee85e26ef9203bfe9
Evtushevskiy Maksim <96142467+Maksimka3168@users.noreply.github.com>
2023-07-13 20:59:48 +0300

    Adding models and databases

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from database.database import session
+from database.models import User
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
+def hello_world():
     return 'Hello World!'
 
 
