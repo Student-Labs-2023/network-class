43f981074feb2637d54a309236bc126b07b6bd60
Evtushevskiy Maksim <96142467+Maksimka3168@users.noreply.github.com>
2023-07-30 23:02:47 +0300

    Add cors

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 from database.database import db
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/channels/*": {"origins": "*"}})
 
 app.config.from_pyfile('config.py')
 db.init_app(app=app)
@@ -19,4 +22,4 @@ with app.app_context():
     print(db.get_engine().engine)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
