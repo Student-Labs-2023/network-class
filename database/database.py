068563270ed327b6d11ef221cb2cc902ad2603be
Daniil Khmelev <Danila_Foxek@mail.ru>
2023-08-03 22:07:38 +0600

    Move to FastAPI

--- database/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine('postgresql://postgres:postgres@localhost/networkclass')
-
-Session = sessionmaker(bind=engine)
-session = Session()
