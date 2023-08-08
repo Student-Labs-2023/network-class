3c9fc4b68534f52e0f5c932040a293860bdf5a24
Daniil Khmelev <Danila_Foxek@mail.ru>
2023-08-08 12:54:10 +0600

    Fix endpoint

--- src/user_channel/router.py
+++ src/user_channel/router.py
@@ -9,9 +9,31 @@ from src.database import get_async_session
 router = APIRouter(
     prefix="/users"
 )
-@router.get("/users/{user_id}/channels/" , response_model=list[Channels])
+@router.get("/{user_id}/channels/" , response_model=list[ChannelResponse])
 async def get_user_channels(user_id: int, session: AsyncSession = Depends(get_async_session)):
-    user_channels = select(UserChannels).where(UserChannels.user_id == user_id)
-    channels = select(Channels).where(Channels.id == user_channels)
-    return channels
+    query = select(User).where(UserChannels.id == user_id)
+    result = await session.execute(query)
+    user_info = result.first()
 
+    if user_info:
+        raise HTTPException(status_code=400, detail="Такого пользователя не существует")
+
+    query = select(UserChannels).where(UserChannels.user_id == user_id)
+    result = await session.execute(query)
+    user_channels = result.fetchall()
+
+    response_list = []
+
+    for channel in user_channels:
+        query = select(Channels).where(Channels.id == user_channels[0].channel_id)
+        result = await session.execute(query)
+        channels = result.first()
+
+        channels_dict = channels.as_dict()
+
+        channels_dict["owner_fullname"] = user_info[0].full_name
+        channels_dict["owner_email"] = user_info[0].email
+
+        response_list.append(channels_dict)
+
+    return response_list
